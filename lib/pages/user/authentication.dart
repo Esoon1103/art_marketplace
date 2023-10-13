60e87f30d36ae14f9b35b72ed934fa2183b50a7e
esoon <p20012522@student.newinti.edu.my>
2023-10-14 00:49:22 +0800

    Complete user request to be seller. Complete settings page ui.
    Working on users can edit username, terminate account, edit address and password
    Working on sellers centre where the users can manage their products.

--- lib/pages/user/authentication.dart
+++ lib/pages/user/authentication.dart
@@ -3,10 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:art_marketplace/widgets/user/bottom_navigation_bar.dart'
-as user_bottom_navigation_bar;
+    as user_bottom_navigation_bar;
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Ensure that Flutter is initialized
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure that Flutter is initialized
   await Firebase.initializeApp();
 
   runApp(const MaterialApp(
@@ -23,25 +24,28 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot){
-          if (snapshot.connectionState == ConnectionState.active) {
-            final User? user = snapshot.data;
-
-            if (user == null) {
-              // User is not logged in, navigate to login page
-              return const UserLogin();
-            } else {
-              // User is logged in, navigate to home page
-              return const user_bottom_navigation_bar.BottomNavigationBar(pageNum: 0);
-            }
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const user_bottom_navigation_bar.BottomNavigationBar(
+                pageNum: 0);
+          } else if (snapshot.data == null) {
+            // User is not logged in, navigate to login page
+            return const UserLogin();
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text('Something Went Wrong :('),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
-
-          return const Center(child: CircularProgressIndicator());
         },
       ),
     );
