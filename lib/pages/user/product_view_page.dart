3524c3b5725c7a316c31752d52fc26e144b85421
esoon <p20012522@student.newinti.edu.my>
2023-10-19 23:53:48 +0800

    Complete display each item of products view page.
    Working on AR for each product that contains 3D image.
    
    Bug
    1. When adding products, the selected image will not updated in the UI. (Yet to fix)

--- lib/pages/user/product_view_page.dart
+++ lib/pages/user/product_view_page.dart
@@ -1,4 +1,6 @@
 import 'package:art_marketplace/model/product_model.dart';
+import 'package:art_marketplace/pages/user/product_view_ar_page.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class ProductViewPage extends StatefulWidget {
@@ -18,106 +20,150 @@ class _ProductViewPageState extends State<ProductViewPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              expandedHeight: MediaQuery.of(context).size.height * 0.6,
-              elevation: 0,
-              snap: true,
-              floating: true,
-              stretch: true,
-              backgroundColor: Colors.grey.shade50,
-              flexibleSpace: FlexibleSpaceBar(
-                  stretchModes: [
-                    StretchMode.zoomBackground,
-                  ],
-                  background: Image.network(widget.product.image, fit: BoxFit.cover,)
-              ),
-              bottom: PreferredSize(
-                  preferredSize: Size.fromHeight(45),
-                  child: Transform.translate(
-                    offset: Offset(0, 1),
-                    child: Container(
-                      height: 45,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30),
-                        ),
-                      ),
-                      child: Center(
-                          child: Container(
-                            width: 50,
-                            height: 8,
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade300,
-                              borderRadius: BorderRadius.circular(10),
+      body: CustomScrollView(slivers: [
+        SliverAppBar(
+          iconTheme: const IconThemeData(color: Colors.grey),
+          expandedHeight: MediaQuery.of(context).size.height * 0.7,
+          elevation: 0,
+          snap: true,
+          floating: true,
+          stretch: true,
+          backgroundColor: Colors.grey.shade50,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.zoomBackground,
+            ],
+            background: Image.network(widget.product.image, fit: BoxFit.cover),
+          ),
+          bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(45),
+              child: Transform.translate(
+                offset: const Offset(0, 1),
+                child: Container(
+                  height: 25,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: Center(
+                      child: Container(
+                    width: 50,
+                    height: 8,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  )),
+                ),
+              )),
+        ),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.product.name,
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          )
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            GestureDetector(
+                              onTap: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => ProductViewARPage(product3DImage: widget.product.image)));
+                              },
+                              child: SizedBox(
+                                height: 30,
+                                width: 90,
+                                child: DottedBorder(
+                                  borderType: BorderType.RRect,
+                                  radius: const Radius.circular(12),
+                                  padding: const EdgeInsets.all(6),
+                                  child: ClipRRect(
+                                      borderRadius:
+                                      const BorderRadius.all(Radius.circular(12)),
+                                      child: Row(children: [
+                                        Image.asset('assets/images/ar_logo.png'),
+                                        const Text(
+                                          "View AR",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ] )),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          "RM ${widget.product.price}.00",
+                          style: const TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      widget.product.description,
+                      style: TextStyle(
+                        height: 1.5,
+                        color: Colors.grey.shade800,
+                        fontSize: 15,
                       ),
                     ),
-                  )
-              ),
-            ),
-            SliverList(
-                delegate: SliverChildListDelegate([
-                  Container(
-                      height: MediaQuery.of(context).size.height * 0.55,
-                      color: Colors.white,
-                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(widget.product.name,
-                                    style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold,),
-                                  ),
-                                  SizedBox(height: 5,),
-                                  Text(widget.product.name, style: TextStyle(color: Colors.orange.shade400, fontSize: 14,),),
-                                ],
-                              ),
-                              Text("\RM " +widget.product.price.toString() + '.00',
-                                style: TextStyle(color: Colors.black, fontSize: 16),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 20,),
-                          Text(widget.product.description,
-                            style: TextStyle(height: 1.5, color: Colors.grey.shade800, fontSize: 15,),
-                          ),
-                          SizedBox(height: 30,),
-
-                          SizedBox(height: 10,),
-                          SizedBox(height: 20,),
-                          MaterialButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            height: 50,
-                            elevation: 0,
-                            splashColor: Colors.yellow[700],
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)
-                            ),
-                            color: Colors.yellow[800],
-                            child: Center(
-                              child: Text("Add to Cart", style: TextStyle(color: Colors.white, fontSize: 18),),
-                            ),
-                          )
-                        ],
-                      )
-                  )
-                ])
-            ),
-          ]
-      ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      height: 50,
+                      elevation: 0,
+                      splashColor: Colors.yellow[700],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      color: Colors.black87,
+                      child: const Center(
+                        child: Text(
+                          "Add to Cart",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                  ],
+                ),
+              ))
+        ])),
+      ]),
     );
   }
 }
