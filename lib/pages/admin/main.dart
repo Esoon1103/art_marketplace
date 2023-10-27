2b17f6d0efea0c16b6167be9276087f32cda7c07
esoon <p20012522@student.newinti.edu.my>
2023-10-28 02:00:56 +0800

    update files new 5
    Success on firebase hosting. Remain only merge
    Working on Admin Panel
    - initialize firebase hosting

--- lib/pages/admin/main.dart
+++ lib/pages/admin/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Test Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Test 123123Page'),
     );
   }
 }
